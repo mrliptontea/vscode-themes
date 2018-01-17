b8a31d135fa11413c879dc8e464c1803362c1fea
mrliptontea <rayman17@gmail.com>
2018-01-17 22:35:36 +0000

    Few minor changes

---
+++ samples/php.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Vendor\Package;
+
+use FooInterface;
+use BarClass as Bar;
+use OtherVendor\OtherPackage\BazClass;
+
+class Foo extends Bar implements FooInterface
+{
+    public function sampleMethod($a, $b = null)
+    {
+        if ($a === $b && $b instanceof Bar) {
+            bar();
+        } elseif ($a > $b) {
+            $foo->bar(42);
+        } else {
+            BazClass::bar('foo', $b);
+        }
+    }
+
+    final public static function bar()
+    {
+        // method body
+    }
+}
